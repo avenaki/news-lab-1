09aa0a7f54248dc87b5e9945563ea3a3bd0b432a
Anastasia1123 <anivshusharina@gmail.com>
2019-10-08 11:49:23 +0300

    Added flask, server works

--- lab_1/Web/server.py
+++ lab_1/Web/server.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from lab_1.HTML_crawler.html_crawler import get_html_page, find_articles, publish_report
+import requests as client
+import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello world!'
+def run_crawler():
+    url = 'https://journal.tinkoff.ru/selected/around-the-world/'
+    html_page = get_html_page(client, url).text
+    articles = find_articles(html_page)
+    creation_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    summary = {"url": url,
+           "creationDate": creation_date,
+           "articles": articles}
+    path = "../HTML_crawler/articles.json"
+    publish_report(path, summary)
+    return render_template('news_page.html', url=url, date=creation_date, articles=articles)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8000)
